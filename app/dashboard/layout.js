1a68c7e491a828edd694efdaa3c39558bd84159b
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2025-02-28 00:28:01 +0530

    Simplify DashboardLayout component by removing unnecessary providers and returning children directly

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,16 +1,3 @@
-import { WebSocketProvider } from "../context/WebSocketContext";
-import StoreProvider from "../StoreProvider";
-
 export default async function DashboardLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <main className="flex-1">
-          <StoreProvider>
-            <WebSocketProvider>{children}</WebSocketProvider>{" "}
-          </StoreProvider>
-        </main>
-      </body>
-    </html>
-  );
+  return children;
 }
